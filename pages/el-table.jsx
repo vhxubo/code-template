e5ec800284486ffd5cb7639c8c955f26897210c9
vhxubo <vhxubo@gmail.com>
2025-09-29 10:26:27 +0800

    perf: el-table gen

--- pages/el-table.jsx
+++ pages/el-table.jsx
@@ -1,25 +1,58 @@
-import { Input, Button, Checkbox } from "antd";
+import { Input, Button, Checkbox, Select } from "antd";
 import { useState } from "react";
-import Handlebars from "handlebars";
-
-const template = Handlebars.compile(
-  `{{#each columns}}
-  <el-table-column prop="{{this.prop}}" label="{{this.label}}" {{attributes ../attrs}}></el-table-column>
-  {{/each}}`,
-);
-Handlebars.registerHelper("attributes", function (attrs, options) {
-  let result = "";
-  for (const attr of attrs) {
-    let value = attr.value;
-    result += ` ${Handlebars.escapeExpression(attr.attr)}="${Handlebars.escapeExpression(value)}"`;
-  }
-  return new Handlebars.SafeString(result); // 注意 SafeString，Handlebars 不会对这段 HTML 进行转义
+import * as nunjucks from "nunjucks";
+const placeholder = `region:行政区划
+lon : 经度
+lat : 纬度
+position : 地理位置
+area : 面积（亩）
+land_type : 土地类型 【1.建设用地 2.农用地 3.未利用地 4.其他】
+ownership : 权属 【1.国有  2. 集体  3.个人】
+usage : 使用情况`;
+nunjucks.configure({
+  trimBlocks: true, // 自动移除块周围的换行
+  lstripBlocks: true, // 移除块开始位置的空白
 });
+
+const templateStr = `<el-table :data="tableData" border>
+  <el-table-column type="index" width="50" label="序号" />
+  <el-table-column type="selection" width="50" />
+
+{%for item in columns %}
+  <el-table-column prop="{{item.prop}}" label="{{item.label}}"{%for attr in attrs %} {{attr.attr}}{% if attr.value  %}="{{attr.value}}"{% endif %}{% endfor %}>
+  {% if item.prop in scopes %}
+  {% raw %}    </el-table>
+      <div>
+        {{ scope.row }}
+      </div>
+    </template>{% endraw %}
+  {% endif %}  </el-table-column>
+  {% endfor %}
+
+  <el-table-column width="100" label="操作">
+  {% raw %}    <template slot-scope="scope">
+      <el-button type="text" @click="handleEdit(scope.row)">编辑</el-button>
+      <el-popconfirm
+        title="确定删除?"
+        @confirm="handleDelete([scope.row])"
+      >
+        <el-button
+          slot="reference"
+          type="text"
+          style="color: #f56c6c; margin-left: 8px"
+          >删除</el-button>
+        </el-popconfirm>
+      </template>
+    </template>{% endraw %}
+  </el-table-column>
+  </el-table>`;
+
 function ElTable() {
-  const [result, setResult] = useState(`dv`);
+  const [result, setResult] = useState(``);
   const [prefix, setPrefix] = useState([]);
   const [columns, setColumns] = useState([]);
   const [attrs, setAttrs] = useState([]);
+  const [scopes, setScopes] = useState(false);
   const onChange = (el) => {
     const text = el.target.value;
     if (!text) {
@@ -32,29 +65,40 @@ function ElTable() {
       prop = prop.trim();
       label = label.trim();
       label = label.split(" ")[0];
-      list.push({ label, prop });
+      list.push({ label, prop, value: prop });
     });
     setColumns(() => list);
   };
-  const onTypeChange = (val) => {};
   const handleSubmit = () => {
-    setResult(template({ columns: columns, attrs: attrs }));
+    setResult(
+      nunjucks.renderString(templateStr, {
+        columns: columns,
+        attrs: attrs,
+        scopes,
+      }),
+    );
   };
-  const plainOptions = ["index", "selection"];
   const columnOptions = [
-    { label: "width", value: "width" },
-    { label: "min-width", value: "min-width" },
-    { label: "align", value: "align" },
-    { label: "header-align", value: "header-align" },
+    { label: "width", value: "width", defaultValue: "80" },
+    { label: "min-width", value: "min-width", defaultValue: "80" },
+    { label: "align", value: "align", defaultValue: "center" },
     { label: "show-overflow-tooltip", value: "show-overflow-tooltip" },
   ];
   const onAttrChange = (val) => {
-    setAttrs(() => val.map((item) => ({ attr: item, value: "1" })));
+    setAttrs(() =>
+      val.map((attr) => {
+        const item = columnOptions.find((o) => o.value === attr);
+        return { attr, value: item.defaultValue };
+      }),
+    );
+  };
+  const onScopeChange = (checked) => {
+    setScopes(checked);
   };
   return (
     <div>
       <Input.TextArea
-        placeholder="textarea with clear icon"
+        placeholder={placeholder}
         allowClear
         rows={10}
         onChange={onChange}
@@ -63,16 +107,21 @@ function ElTable() {
         生成
       </Button>
       <div>
-        <Checkbox.Group
-          options={plainOptions}
-          defaultValue={["Apple"]}
-          onChange={onTypeChange}
-        />
-      </div>
-      <div>
         <Checkbox.Group options={columnOptions} onChange={onAttrChange} />
+        <div className="flex gap-3 items-center">
+          模板
+          <Select
+            allowClear
+            style={{ minWidth: "200px" }}
+            mode="multiple"
+            onChange={onScopeChange}
+            options={columns}
+          />
+        </div>
       </div>
-      {result}
+      <pre>
+        <code>{result}</code>
+      </pre>
     </div>
   );
 }
